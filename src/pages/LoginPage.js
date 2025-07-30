1e6d6e75b9b08fcdcaaabc381d68fdfe897ae356
ramyabasava <150227151+ramyabasava@users.noreply.github.com>
2025-07-30 10:48:26 +0530

    Update LoginPage.js

--- src/pages/LoginPage.js
+++ src/pages/LoginPage.js
@@ -3,7 +3,7 @@ import { Link, useNavigate, useLocation } from 'react-router-dom';
 import axios from 'axios';
 import { useAuth } from '../context/AuthContext'; // Import useAuth
 
-const API_BASE_URL = 'http://127.0.0.1:5000/api';
+const API_BASE_URL = process.env.REACT_APP_API_URL || 'http://127.0.0.1:5000/api';
 
 const LoginPage = () => {
   const [email, setEmail] = useState('');
