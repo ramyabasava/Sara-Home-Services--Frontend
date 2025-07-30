8b3b9e7d5143cc180954c8f12f66793b30f5fc68
ramyabasava <150227151+ramyabasava@users.noreply.github.com>
2025-07-30 10:47:57 +0530

    Update RegisterPage.js

--- src/pages/RegisterPage.js
+++ src/pages/RegisterPage.js
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 import axios from 'axios';
 
-const API_BASE_URL = 'http://127.0.0.1:5000/api';
+const API_BASE_URL = process.env.REACT_APP_API_URL || 'http://127.0.0.1:5000/api';
 
 const RegisterPage = () => {
   const [email, setEmail] = useState('');
